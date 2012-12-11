78ae15693e6a8b1f103c73e91c804459464af45d
HIGUCHI Yuta <y-higuchi@ak.jp.nec.com>
2012-12-11 16:39:04 +0900

    Add test case for get all link,port,switch Add test case for set discovered link

--- unittests/topology/service_management_test.c
+++ unittests/topology/service_management_test.c
@@ -15,6 +15,7 @@
 #include "service_management.h"
 
 #include "subscriber_table.h"
+#include "topology_table.h"
 
 /********************************************************************************
  * Common function.
@@ -258,6 +259,96 @@ mock_topology_reply( uint16_t tag, void *data, size_t len, void *user_data ) {
 }
 
 static void
+mock_query_link_status_reply( uint16_t tag,
+                              void *data, size_t len,
+                              void *param0 ) {
+  UNUSED( tag );
+  UNUSED( param0 );
+  topology_link_status *const link_status = data;
+  const int number_of_links =( int ) ( len / sizeof( topology_link_status ) );
+  check_expected( number_of_links );
+
+  // rearrange byte order
+  for (int  i = 0; i < number_of_links; i++ ) {
+    topology_link_status *s = &link_status[ i ];
+    s->from_dpid = ntohll( s->from_dpid );
+    uint64_t from_dpid = s->from_dpid;
+    check_expected( from_dpid );
+    s->from_portno = ntohs( s->from_portno );
+    uint16_t from_portno = s->from_portno;
+    check_expected( from_portno );
+    s->to_dpid = ntohll( s->to_dpid );
+    uint64_t to_dpid = s->to_dpid;
+    check_expected( to_dpid );
+    s->to_portno = ntohs( s->to_portno );
+    uint16_t to_portno = s->to_portno;
+    check_expected( to_portno );
+
+    uint8_t status = s->status;
+    check_expected( status );
+  }
+}
+
+
+// handle reply from topology
+static void
+mock_query_port_status_reply( uint16_t tag,
+                              void *data, size_t len,
+                              void *param0 ) {
+  UNUSED( tag );
+  UNUSED( param0 );
+  topology_port_status *const port_status = data;
+  const int number_of_ports =( int ) ( len / sizeof( topology_port_status ) );
+  check_expected( number_of_ports );
+
+  // rearrange byte order
+  for ( int i = 0; i < number_of_ports; i++ ) {
+    topology_port_status *s = &port_status[ i ];
+    s->dpid = ntohll( s->dpid );
+    uint64_t dpid = s->dpid;
+    check_expected( dpid );
+    s->port_no = ntohs( s->port_no );
+    uint16_t port_no = s->port_no;
+    check_expected( port_no );
+
+    uint8_t status = s->status;
+    check_expected( status );
+    uint8_t external = s->external;
+    check_expected( external );
+
+    const char* name = s->name;
+    check_expected( name );
+
+    uint8_t* mac = s->mac;
+    check_expected( mac );
+  }
+}
+
+
+// handle reply from topology
+static void
+mock_query_switch_status_reply( uint16_t tag,
+                                void *data, size_t len, void *param0 ) {
+  UNUSED( tag );
+  UNUSED( param0 );
+  topology_switch_status *const switch_status = data;
+  const int number_of_switches = ( int ) ( len / sizeof( topology_switch_status ) );
+  check_expected( number_of_switches );
+
+  // rearrange byte order
+  for ( int i = 0; i < number_of_switches; i++ ) {
+    topology_switch_status *s = &switch_status[ i ];
+    s->dpid = ntohll( s->dpid );
+    uint64_t dpid = s->dpid;
+    check_expected( dpid );
+
+    uint8_t status = s->status;
+    check_expected( status );
+  }
+}
+
+
+static void
 callback_fake_libtopology_client_reply_end( uint16_t tag, void *data, size_t len, void *user_data ) {
   switch ( tag ) {
   case TD_MSGTYPE_SUBSCRIBE_RESPONSE:
@@ -265,6 +356,18 @@ callback_fake_libtopology_client_reply_end( uint16_t tag, void *data, size_t len
   case TD_MSGTYPE_ENABLE_DISCOVERY_RESPONSE:
   case TD_MSGTYPE_DISABLE_DISCOVERY_RESPONSE:
     mock_topology_reply( tag, data, len, user_data );
+    break;
+
+  case TD_MSGTYPE_QUERY_LINK_STATUS_RESPONSE:
+    mock_query_link_status_reply( tag, data, len, user_data );
+    break;
+
+  case TD_MSGTYPE_QUERY_PORT_STATUS_RESPONSE:
+    mock_query_port_status_reply( tag, data, len, user_data );
+    break;
+
+  case TD_MSGTYPE_QUERY_SWITCH_STATUS_RESPONSE:
+    mock_query_switch_status_reply( tag, data, len, user_data );
     break;
 
   default:
@@ -581,6 +684,9 @@ test_set_link_status_updated_hook() {
   expect_value( local_link_status_updated_handler, link_up, true );
 
   notify_link_status_for_all_user( &port );
+
+  assert_true( set_link_status_updated_hook( NULL, NULL ) );
+
 }
 
 
@@ -634,6 +740,9 @@ test_set_port_status_updated_hook() {
   expect_value( local_port_status_updated_handler, external, false );
 
   notify_port_status_for_all_user( &port );
+
+  assert_true( set_port_status_updated_hook( NULL, NULL ) );
+
 }
 
 //bool set_switch_status_updated_hook( switch_status_updated_hook, void *user_data );
@@ -660,9 +769,11 @@ test_set_switch_status_updated_hook() {
   expect_value( local_switch_status_updated_handler, datapath_id, 0x1234 );
   expect_value( local_switch_status_updated_handler, up, true );
   notify_switch_status_for_all_user( &sw );
+
+  assert_true( set_switch_status_updated_hook( NULL, NULL ) );
+
 }
 
-//uint8_t set_discovered_link_status( topology_update_link_status* link_status );
 
 
 
@@ -1010,6 +1121,286 @@ test_disable_discovery_from_client() {
   revert_original( execute_timer_events );
 }
 
+static void
+test_query_switch_status_from_client() {
+  // avoid periodic ping event from running.
+  void ( *original_execute_timer_events )( int *next_timeout_usec );
+  swap_original( execute_timer_events );
+
+  init_messenger( "/tmp" );
+  init_timer();
+
+  assert_true( add_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( add_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  start_service_management();
+
+  uint64_t dpid = 0x1234;
+  sw_entry* sw = update_sw_entry( &dpid );
+  sw->up = false;
+
+  // send subscribe request
+  const size_t req_len = strlen( TEST_SUBSCRIBER_NAME ) + 1;
+  buffer *buf = alloc_buffer_with_length( req_len );
+  topology_request *req = append_back_buffer( buf, req_len );
+  strcpy( req->name, TEST_SUBSCRIBER_NAME );
+  send_request_message( TEST_TREMA_NAME, TEST_SUBSCRIBER_NAME, TD_MSGTYPE_QUERY_SWITCH_STATUS_REQUEST,
+                        buf->data, buf->length, NULL );
+  free_buffer( buf );
+
+
+  expect_value( mock_query_switch_status_reply, number_of_switches, 1 );
+  expect_value( mock_query_switch_status_reply, status, TD_SWITCH_DOWN );
+  expect_value( mock_query_switch_status_reply, dpid, 0x1234 );
+
+  start_event_handler();
+  start_messenger();
+
+  delete_sw_entry( sw );
+
+  stop_service_management();
+
+  assert_true( delete_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( delete_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  finalize_timer();
+  finalize_messenger();
+
+  revert_original( execute_timer_events );
+}
+
+static void
+test_query_port_status_from_client() {
+  // avoid periodic ping event from running.
+  void ( *original_execute_timer_events )( int *next_timeout_usec );
+  swap_original( execute_timer_events );
+
+  init_messenger( "/tmp" );
+  init_timer();
+
+  assert_true( add_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( add_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  start_service_management();
+
+  uint64_t dpid = 0x1234;
+  sw_entry* sw = update_sw_entry( &dpid );
+  sw->up = false;
+  port_entry* port = update_port_entry( sw, 42, "Some port name" );
+  port->external = true;
+  for( int i = 0 ; i < ETH_ADDRLEN ; ++i ){
+    port->mac[i] = (uint8_t)i;
+  }
+  port->up = false;
+
+  // send subscribe request
+  const size_t req_len = strlen( TEST_SUBSCRIBER_NAME ) + 1;
+  buffer *buf = alloc_buffer_with_length( req_len );
+  topology_request *req = append_back_buffer( buf, req_len );
+  strcpy( req->name, TEST_SUBSCRIBER_NAME );
+  send_request_message( TEST_TREMA_NAME, TEST_SUBSCRIBER_NAME, TD_MSGTYPE_QUERY_PORT_STATUS_REQUEST,
+                        buf->data, buf->length, NULL );
+  free_buffer( buf );
+
+
+  expect_value( mock_query_port_status_reply, number_of_ports, 1 );
+  expect_value( mock_query_port_status_reply, status, TD_PORT_DOWN );
+  expect_value( mock_query_port_status_reply, dpid, 0x1234 );
+  expect_value( mock_query_port_status_reply, port_no, 42 );
+  expect_value( mock_query_port_status_reply, external, TD_PORT_EXTERNAL );
+  expect_string( mock_query_port_status_reply, name, "Some port name" );
+  expect_memory( mock_query_port_status_reply, mac, "\0\1\2\3\4\5", ETH_ADDRLEN );
+
+  start_event_handler();
+  start_messenger();
+
+  delete_port_entry( sw, port );
+  delete_sw_entry( sw );
+
+  stop_service_management();
+
+  assert_true( delete_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( delete_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  finalize_timer();
+  finalize_messenger();
+
+  revert_original( execute_timer_events );
+}
+
+static void
+test_query_link_status_from_client() {
+  // avoid periodic ping event from running.
+  void ( *original_execute_timer_events )( int *next_timeout_usec );
+  swap_original( execute_timer_events );
+
+  init_messenger( "/tmp" );
+  init_timer();
+
+  assert_true( add_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( add_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  start_service_management();
+
+  uint64_t dpid = 0x1234;
+  sw_entry* sw = update_sw_entry( &dpid );
+  sw->up = true;
+  dpid = 0x5678;
+  sw_entry* sw2 = update_sw_entry( &dpid );
+  sw2->up = true;
+
+  port_entry* port = update_port_entry( sw, 42, "Some port name1" );
+  port->external = false;
+  for( int i = 0 ; i < ETH_ADDRLEN ; ++i ){
+    port->mac[i] = (uint8_t)i;
+  }
+  port->up = true;
+
+  port_entry* port2 = update_port_entry( sw2, 72, "Some port name2" );
+  port2->external = false;
+  for( int i = 0 ; i < ETH_ADDRLEN ; ++i ){
+    port2->mac[i] = (uint8_t)i;
+  }
+  port2->up = true;
+
+  dpid = 0x5678;
+  update_link_to( port, &dpid, 72, true );
+
+  dpid = 0x1234;
+  update_link_to( port2, &dpid, 42, true );
+
+  // send subscribe request
+  const size_t req_len = strlen( TEST_SUBSCRIBER_NAME ) + 1;
+  buffer *buf = alloc_buffer_with_length( req_len );
+  topology_request *req = append_back_buffer( buf, req_len );
+  strcpy( req->name, TEST_SUBSCRIBER_NAME );
+  send_request_message( TEST_TREMA_NAME, TEST_SUBSCRIBER_NAME, TD_MSGTYPE_QUERY_LINK_STATUS_REQUEST,
+                        buf->data, buf->length, NULL );
+  free_buffer( buf );
+
+
+  expect_value( mock_query_link_status_reply, number_of_links, 2 );
+
+  expect_value( mock_query_link_status_reply, status, TD_LINK_UP );
+  expect_value( mock_query_link_status_reply, to_dpid, 0x1234 );
+  expect_value( mock_query_link_status_reply, to_portno, 42 );
+  expect_value( mock_query_link_status_reply, from_dpid, 0x5678 );
+  expect_value( mock_query_link_status_reply, from_portno, 72 );
+
+  expect_value( mock_query_link_status_reply, status, TD_LINK_UP );
+  expect_value( mock_query_link_status_reply, from_dpid, 0x1234 );
+  expect_value( mock_query_link_status_reply, from_portno, 42 );
+  expect_value( mock_query_link_status_reply, to_dpid, 0x5678 );
+  expect_value( mock_query_link_status_reply, to_portno, 72 );
+
+
+
+  start_event_handler();
+  start_messenger();
+
+  delete_link_to( port );
+  delete_link_to( port2 );
+  delete_port_entry( sw, port );
+  delete_port_entry( sw2, port2 );
+  delete_sw_entry( sw );
+  delete_sw_entry( sw2 );
+
+  stop_service_management();
+
+  assert_true( delete_message_replied_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_reply_end ) );
+//  assert_true( delete_message_received_callback( TEST_CONTROL_NAME, callback_test_control_notification_handler ) );
+
+  finalize_timer();
+  finalize_messenger();
+
+  revert_original( execute_timer_events );
+}
+
+
+//uint8_t set_discovered_link_status( topology_update_link_status* link_status );
+static void
+test_set_discovered_link_status() {
+
+  init_messenger( "/tmp" );
+  init_timer();
+
+  assert_true( add_message_received_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_notification_end ) );
+
+
+  assert_true( set_port_status_updated_hook( local_port_status_updated_handler, NULL ) );
+  assert_true( set_link_status_updated_hook( local_link_status_updated_handler, NULL ) );
+
+
+  uint64_t dpid = 0x1234;
+  sw_entry* sw = update_sw_entry( &dpid );
+  sw->up = true;
+  dpid = 0x5678;
+  sw_entry* sw2 = update_sw_entry( &dpid );
+  sw2->up = true;
+
+  port_entry* port = update_port_entry( sw, 42, "Some port name1" );
+  port->external = false;
+  for( int i = 0 ; i < ETH_ADDRLEN ; ++i ){
+    port->mac[i] = (uint8_t)i;
+  }
+  port->up = true;
+
+  port_entry* port2 = update_port_entry( sw2, 72, "Some port name2" );
+  port2->external = false;
+  for( int i = 0 ; i < ETH_ADDRLEN ; ++i ){
+    port2->mac[i] = (uint8_t)i;
+  }
+  port2->up = true;
+
+
+  topology_update_link_status link_status;
+  link_status.from_dpid = 0x1234;
+  link_status.from_portno = 42;
+  link_status.to_dpid = 0x5678;
+  link_status.to_portno = 72;
+  link_status.status = TD_LINK_UP;
+
+
+
+  expect_value( mock_link_status_notification, from_dpid, 0x1234 );
+  expect_value( mock_link_status_notification, from_portno, 42 );
+  expect_value( mock_link_status_notification, to_dpid, 0x5678 );
+  expect_value( mock_link_status_notification, to_portno, 72 );
+  expect_value( mock_link_status_notification, status, TD_LINK_UP );
+
+
+  expect_value( local_link_status_updated_handler, user_data, NULL );
+  expect_not_value( local_link_status_updated_handler, sw, NULL );
+  expect_value( local_link_status_updated_handler, port_no, 42 );
+  expect_string( local_link_status_updated_handler, name, "Some port name1" );
+  expect_memory( local_link_status_updated_handler, mac, "\0\1\2\3\4\5", ETH_ADDRLEN );
+  expect_value( local_link_status_updated_handler, up, true );
+  expect_value( local_link_status_updated_handler, external, false );
+  expect_value( local_link_status_updated_handler, link_dpid, 0x5678 );
+  expect_value( local_link_status_updated_handler, link_port_no, 72 );
+  expect_value( local_link_status_updated_handler, link_up, true );
+
+  uint8_t result = set_discovered_link_status( &link_status );
+  assert_int_equal( result, TD_RESPONSE_OK );
+
+  start_event_handler();
+  start_messenger();
+
+  delete_link_to( port );
+  delete_link_to( port2 );
+  delete_port_entry( sw, port );
+  delete_port_entry( sw2, port2 );
+  delete_sw_entry( sw );
+  delete_sw_entry( sw2 );
+
+  assert_true( set_port_status_updated_hook( NULL, NULL ) );
+  assert_true( set_link_status_updated_hook( NULL, NULL ) );
+  assert_true( delete_message_received_callback( TEST_SUBSCRIBER_NAME, callback_fake_libtopology_client_notification_end ) );
+
+  finalize_timer();
+  finalize_messenger();
+}
+
 /********************************************************************************
  * Run tests.
  ********************************************************************************/
@@ -1039,6 +1430,11 @@ main() {
       unit_test_setup_teardown( test_enable_discovery_from_client, setup_fake_subscriber, teardown_fake_subscriber ),
       unit_test_setup_teardown( test_disable_discovery_from_client, setup_fake_subscriber, teardown_fake_subscriber ),
 
+      unit_test_setup_teardown( test_query_switch_status_from_client, setup_service_management, teardown_service_management ),
+      unit_test_setup_teardown( test_query_port_status_from_client, setup_service_management, teardown_service_management ),
+      unit_test_setup_teardown( test_query_link_status_from_client, setup_service_management, teardown_service_management ),
+
+      unit_test_setup_teardown( test_set_discovered_link_status, setup_fake_subscriber, teardown_fake_subscriber ),
   };
 
   setup_leak_detector();
