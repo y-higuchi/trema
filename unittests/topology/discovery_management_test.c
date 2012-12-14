d9950e3473c475a1fdaf7ba52b49590c9cc92d3e
HIGUCHI Yuta <y-higuchi@ak.jp.nec.com>
2012-12-14 16:12:12 +0900

    Added test cases for discovery mgmt

--- unittests/topology/discovery_management_test.c
+++ unittests/topology/discovery_management_test.c
@@ -294,10 +294,8 @@ test_disable_discovery_twice_prints_message() {
 
 
 static void
-helper_sw_message_received_end( uint16_t tag, void *data, size_t len ) {
+helper_sw_received_flow_mod_add_lldp_message_end( uint16_t tag, void *data, size_t len ) {
   check_expected( tag );
-  UNUSED( data );
-  UNUSED( len );
 
   buffer* buffer = alloc_buffer_with_length(len);
   void* raw_data = append_back_buffer( buffer, len );
@@ -332,7 +330,7 @@ helper_sw_message_received_end( uint16_t tag, void *data, size_t len ) {
 
 
 static void
-test_switch_status_event() {
+test_switch_status_event_then_flow_mod_lldp_if_sw_up() {
   setup_discovery_mgmt();
   expect_switch_and_port_status_hook_set();
   enable_discovery();
@@ -350,12 +348,12 @@ test_switch_status_event() {
   // Test: send LLDP flow mod if sw up
 
   // dummy OFA receiving notification
-  UNUSED( helper_sw_message_received_end );
+  UNUSED( helper_sw_received_flow_mod_add_lldp_message_end );
   const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
-  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_message_received_end ) );
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_message_end ) );
 
-  expect_value( helper_sw_message_received_end, tag, MESSENGER_OPENFLOW_MESSAGE );
-  expect_value( helper_sw_message_received_end, datapath_id, 0x1234 );
+  expect_value( helper_sw_received_flow_mod_add_lldp_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_add_lldp_message_end, datapath_id, 0x1234 );
 
 
   sw.up = true;
@@ -365,7 +363,7 @@ test_switch_status_event() {
   start_event_handler();
 
 
-  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_message_received_end ) );
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_message_end ) );
 
   expect_switch_and_port_status_hook_clear();
   disable_discovery();
@@ -375,10 +373,8 @@ test_switch_status_event() {
 
 
 static void
-helper_sw_message_received_over_ip_end( uint16_t tag, void *data, size_t len ) {
+helper_sw_received_flow_mod_add_lldp_over_ip_message_end( uint16_t tag, void *data, size_t len ) {
   check_expected( tag );
-  UNUSED( data );
-  UNUSED( len );
 
   buffer* buffer = alloc_buffer_with_length(len);
   void* raw_data = append_back_buffer( buffer, len );
@@ -418,7 +414,7 @@ helper_sw_message_received_over_ip_end( uint16_t tag, void *data, size_t len ) {
 
 
 static void
-test_switch_status_event_over_ip() {
+test_switch_status_event_over_ip_then_flow_mod_lldp_if_sw_up() {
   setup();
   discovery_management_options options;
   options.always_enabled = false;
@@ -445,14 +441,14 @@ test_switch_status_event_over_ip() {
   // Test: send LLDP flow mod if sw up
 
   // dummy OFA receiving notification
-  UNUSED( helper_sw_message_received_end );
+  UNUSED( helper_sw_received_flow_mod_add_lldp_message_end );
   const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
-  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_message_received_over_ip_end ) );
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_over_ip_message_end ) );
 
-  expect_value( helper_sw_message_received_over_ip_end, tag, MESSENGER_OPENFLOW_MESSAGE );
-  expect_value( helper_sw_message_received_over_ip_end, datapath_id, 0x1234 );
-  expect_value( helper_sw_message_received_over_ip_end, nw_src, 0x01234567 );
-  expect_value( helper_sw_message_received_over_ip_end, nw_dst, 0x89ABCDEF );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, datapath_id, 0x1234 );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, nw_src, 0x01234567 );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, nw_dst, 0x89ABCDEF );
 
 
   sw.up = true;
@@ -462,7 +458,7 @@ test_switch_status_event_over_ip() {
   start_event_handler();
 
 
-  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_message_received_over_ip_end ) );
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_over_ip_message_end ) );
 
   expect_switch_and_port_status_hook_clear();
   disable_discovery();
@@ -512,6 +508,233 @@ test_port_status_event() {
   revert_original( execute_timer_events );
 }
 
+static void
+helper_sw_received_flow_mod_del_lldp_message_end( uint16_t tag, void *data, size_t len ) {
+  check_expected( tag );
+
+  buffer* buffer = alloc_buffer_with_length(len);
+  void* raw_data = append_back_buffer( buffer, len );
+  memcpy( raw_data, data, len );
+
+  openflow_service_header_t* of_s_h = data;
+  const uint64_t datapath_id = ntohll( of_s_h->datapath_id );
+  check_expected( datapath_id );
+  const size_t ofs_header_length = sizeof(openflow_service_header_t) + ntohs( of_s_h->service_name_length );
+
+  struct ofp_flow_mod* ofp_flow_mod = remove_front_buffer(buffer, ofs_header_length );
+  assert_int_equal( ofp_flow_mod->header.type, OFPT_FLOW_MOD );
+  assert_int_equal( ntohs(ofp_flow_mod->priority), UINT16_MAX );
+  assert_int_equal( ntohs(ofp_flow_mod->command), OFPFC_DELETE );
+  assert_int_equal( ntohs(ofp_flow_mod->idle_timeout), 0 );
+  assert_int_equal( ntohs(ofp_flow_mod->hard_timeout), 0 );
+
+  struct ofp_match match;
+  ntoh_match( &match, &ofp_flow_mod->match );
+  assert_int_equal( match.wildcards, OFPFW_ALL & ~OFPFW_DL_TYPE );
+  assert_int_equal( match.dl_type, ETH_ETHTYPE_LLDP );
+
+  struct ofp_action_output* act_out = remove_front_buffer(buffer, offsetof( struct ofp_flow_mod, actions ) );
+  assert_int_equal( ntohs(act_out->type), OFPAT_OUTPUT );
+  assert_int_equal( ntohs(act_out->port), OFPP_CONTROLLER );
+
+  free_buffer( buffer );
+
+  stop_event_handler();
+  stop_messenger();
+}
+
+
+static void
+test_enable_discovery_when_sw_exist_then_flow_mod_add_lldp() {
+  setup_discovery_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry* sw = update_sw_entry( &datapath_id );
+  sw->up = true;
+
+  // dummy OFA receiving notification
+  const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_message_end ) );
+
+  expect_value( helper_sw_received_flow_mod_add_lldp_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_add_lldp_message_end, datapath_id, 0x1234 );
+
+  expect_switch_and_port_status_hook_set();
+  enable_discovery();
+
+  start_messenger();
+  start_event_handler();
+
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_message_end ) );
+
+  delete_sw_entry( sw );
+
+  expect_switch_and_port_status_hook_clear();
+  disable_discovery();
+
+  teardown_discovery_mgmt();
+}
+
+static void
+test_disable_discovery_when_sw_exist_then_flow_mod_del_lldp() {
+  setup_discovery_mgmt();
+
+  // dummy OFA receiving notification
+  const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_del_lldp_message_end ) );
+
+  expect_value( helper_sw_received_flow_mod_del_lldp_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_del_lldp_message_end, datapath_id, 0x1234 );
+
+  expect_switch_and_port_status_hook_set();
+  enable_discovery();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry* sw = update_sw_entry( &datapath_id );
+  sw->up = true;
+
+  expect_switch_and_port_status_hook_clear();
+  disable_discovery();
+
+  start_messenger();
+  start_event_handler();
+
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_del_lldp_message_end ) );
+
+  delete_sw_entry( sw );
+
+  teardown_discovery_mgmt();
+}
+
+
+static void
+helper_sw_received_flow_mod_del_lldp_over_ip_message_end( uint16_t tag, void *data, size_t len ) {
+  check_expected( tag );
+
+  buffer* buffer = alloc_buffer_with_length(len);
+  void* raw_data = append_back_buffer( buffer, len );
+  memcpy( raw_data, data, len );
+
+  openflow_service_header_t* of_s_h = data;
+  const uint64_t datapath_id = ntohll( of_s_h->datapath_id );
+  check_expected( datapath_id );
+  const size_t ofs_header_length = sizeof(openflow_service_header_t) + ntohs( of_s_h->service_name_length );
+
+  struct ofp_flow_mod* ofp_flow_mod = remove_front_buffer(buffer, ofs_header_length );
+  assert_int_equal( ofp_flow_mod->header.type, OFPT_FLOW_MOD );
+  assert_int_equal( ntohs(ofp_flow_mod->priority), UINT16_MAX );
+  assert_int_equal( ntohs(ofp_flow_mod->command), OFPFC_DELETE );
+  assert_int_equal( ntohs(ofp_flow_mod->idle_timeout), 0 );
+  assert_int_equal( ntohs(ofp_flow_mod->hard_timeout), 0 );
+
+  struct ofp_match match;
+  ntoh_match( &match, &ofp_flow_mod->match );
+  assert_int_equal( match.wildcards, OFPFW_ALL & ~( OFPFW_DL_TYPE | OFPFW_NW_PROTO | OFPFW_NW_SRC_MASK | OFPFW_NW_DST_MASK ) );
+  assert_int_equal( match.dl_type, ETH_ETHTYPE_IPV4 );
+  assert_int_equal( match.nw_proto, IPPROTO_ETHERIP );
+  const uint32_t nw_src = match.nw_src;
+  const uint32_t nw_dst = match.nw_dst;
+  check_expected( nw_src );
+  check_expected( nw_dst );
+
+  struct ofp_action_output* act_out = remove_front_buffer(buffer, offsetof( struct ofp_flow_mod, actions ) );
+  assert_int_equal( ntohs(act_out->type), OFPAT_OUTPUT );
+  assert_int_equal( ntohs(act_out->port), OFPP_CONTROLLER );
+
+  free_buffer( buffer );
+
+  stop_event_handler();
+  stop_messenger();
+}
+
+
+static void
+test_enable_discovery_when_sw_exist_then_flow_mod_add_lldp_over_ip() {
+  setup();
+  discovery_management_options options;
+  options.always_enabled = false;
+  options.lldp.lldp_over_ip = true;
+  options.lldp.lldp_ip_src = 0x01234567;
+  options.lldp.lldp_ip_dst = 0x89ABCDEF;
+
+  assert_true( init_discovery_management( options ) );
+  assert_true( start_discovery_management() );
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry* sw = update_sw_entry( &datapath_id );
+  sw->up = true;
+
+  // dummy OFA receiving notification
+  UNUSED( helper_sw_received_flow_mod_add_lldp_message_end );
+  const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_over_ip_message_end ) );
+
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, datapath_id, 0x1234 );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, nw_src, 0x01234567 );
+  expect_value( helper_sw_received_flow_mod_add_lldp_over_ip_message_end, nw_dst, 0x89ABCDEF );
+
+  expect_switch_and_port_status_hook_set();
+  enable_discovery();
+
+  start_messenger();
+  start_event_handler();
+
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_add_lldp_over_ip_message_end ) );
+
+  delete_sw_entry( sw );
+
+  expect_switch_and_port_status_hook_clear();
+  disable_discovery();
+
+  teardown_discovery_mgmt();
+}
+
+
+static void
+test_disable_discovery_when_sw_exist_then_flow_mod_del_lldp_over_ip() {
+  setup();
+  discovery_management_options options;
+  options.always_enabled = false;
+  options.lldp.lldp_over_ip = true;
+  options.lldp.lldp_ip_src = 0x01234567;
+  options.lldp.lldp_ip_dst = 0x89ABCDEF;
+
+  assert_true( init_discovery_management( options ) );
+  assert_true( start_discovery_management() );
+
+
+  // dummy OFA receiving notification
+  UNUSED( helper_sw_received_flow_mod_add_lldp_message_end );
+  const char* SRC_SW_MSNGER_NAME = "switch.0x1234";
+  assert_true( add_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_del_lldp_over_ip_message_end ) );
+
+  expect_value( helper_sw_received_flow_mod_del_lldp_over_ip_message_end, tag, MESSENGER_OPENFLOW_MESSAGE );
+  expect_value( helper_sw_received_flow_mod_del_lldp_over_ip_message_end, datapath_id, 0x1234 );
+  expect_value( helper_sw_received_flow_mod_del_lldp_over_ip_message_end, nw_src, 0x01234567 );
+  expect_value( helper_sw_received_flow_mod_del_lldp_over_ip_message_end, nw_dst, 0x89ABCDEF );
+
+  expect_switch_and_port_status_hook_set();
+  enable_discovery();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry* sw = update_sw_entry( &datapath_id );
+  sw->up = true;
+
+  expect_switch_and_port_status_hook_clear();
+  disable_discovery();
+
+  start_messenger();
+  start_event_handler();
+
+  assert_true( delete_message_received_callback( SRC_SW_MSNGER_NAME, helper_sw_received_flow_mod_del_lldp_over_ip_message_end ) );
+
+  delete_sw_entry( sw );
+
+
+  teardown_discovery_mgmt();
+}
+
 
 /********************************************************************************
  * Run tests.
@@ -525,11 +748,14 @@ main() {
       unit_test_setup_teardown( test_enable_discovery_twice_prints_message, setup_discovery_mgmt, teardown_discovery_mgmt ),
       unit_test_setup_teardown( test_disable_discovery_twice_prints_message, setup_discovery_mgmt, teardown_discovery_mgmt ),
 
-      // TODO enable discovery when sw_entry present then flow del
-      // TODO disable discovery when sw_entry present then flow del
+      unit_test( test_enable_discovery_when_sw_exist_then_flow_mod_add_lldp ),
+      unit_test( test_enable_discovery_when_sw_exist_then_flow_mod_add_lldp_over_ip ),
 
-      unit_test( test_switch_status_event ),
-      unit_test( test_switch_status_event_over_ip ),
+      unit_test( test_disable_discovery_when_sw_exist_then_flow_mod_del_lldp ),
+      unit_test( test_disable_discovery_when_sw_exist_then_flow_mod_del_lldp_over_ip ),
+
+      unit_test( test_switch_status_event_then_flow_mod_lldp_if_sw_up ),
+      unit_test( test_switch_status_event_over_ip_then_flow_mod_lldp_if_sw_up ),
       unit_test_setup_teardown( test_port_status_event, setup_discovery_mgmt, teardown_discovery_mgmt ),
   };
 
