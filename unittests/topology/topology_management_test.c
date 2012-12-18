4f4ed6dd0ca2706658b4370109e33ebf5cda59fd
HIGUCHI Yuta <y-higuchi@ak.jp.nec.com>
2012-12-18 11:31:06 +0900

    Add test cases

--- unittests/topology/topology_management_test.c
+++ unittests/topology/topology_management_test.c
@@ -36,7 +36,10 @@
 
 static void ( *original_notify_switch_status_for_all_user )( sw_entry *sw );
 static void ( *original_notify_port_status_for_all_user )( port_entry *port );
+static void ( *original_notify_link_status_for_all_user )( port_entry *port );
 
+static int mock_notify_switch_status_for_all_user_calls = 0;
+static int mock_notify_switch_status_for_all_user_max = 0;
 
 static void
 mock_notify_switch_status_for_all_user( sw_entry *sw ) {
@@ -45,7 +48,13 @@ mock_notify_switch_status_for_all_user( sw_entry *sw ) {
 
   const bool up = sw->up;
   check_expected( up );
+
+  if( ++mock_notify_switch_status_for_all_user_calls ==  mock_notify_switch_status_for_all_user_max ){
+    stop_event_handler();
+    stop_messenger();
+  }
 }
+
 
 static int mock_notify_port_status_for_all_user_calls = 0;
 static int mock_notify_port_status_for_all_user_max = 0;
@@ -70,6 +79,24 @@ mock_notify_port_status_for_all_user( port_entry *port ) {
   }
 }
 
+static void
+mock_notify_link_status_for_all_user( port_entry *port ) {
+  assert_true( port->sw != NULL );
+  assert_true( port->link_to != NULL );
+
+  const uint64_t from_dpid = port->sw->datapath_id;
+  check_expected( from_dpid );
+  const uint16_t from_portno = port->port_no;
+  check_expected( from_portno );
+
+  const uint64_t to_dpid = port->link_to->datapath_id;
+  check_expected( to_dpid );
+  const uint16_t to_portno = port->link_to->port_no;
+  check_expected( to_portno );
+
+  const bool up = port->link_to->up;
+  check_expected( up );
+}
 
 /********************************************************************************
  * Setup and teardown functions.
@@ -87,6 +114,11 @@ setup() {
 
   swap_original( notify_switch_status_for_all_user );
   swap_original( notify_port_status_for_all_user );
+  swap_original( notify_link_status_for_all_user );
+  mock_notify_switch_status_for_all_user_calls = 0;
+  mock_notify_switch_status_for_all_user_max = 0;
+  mock_notify_port_status_for_all_user_calls = 0;
+  mock_notify_port_status_for_all_user_max = 0;
 }
 
 static void
@@ -98,6 +130,9 @@ teardown() {
 
   revert_original( notify_switch_status_for_all_user );
   revert_original( notify_port_status_for_all_user );
+  revert_original( notify_link_status_for_all_user );
+  mock_notify_switch_status_for_all_user_calls = 0;
+  mock_notify_switch_status_for_all_user_max = 0;
   mock_notify_port_status_for_all_user_calls = 0;
   mock_notify_port_status_for_all_user_max = 0;
 }
@@ -255,11 +290,6 @@ test_feature_reply_then_update_ports() {
   expect_value( mock_notify_port_status_for_all_user, up, false );
 
   // Note: Port name change is not Topology change => Port 1 will not be notified.
-//  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
-//  expect_value( mock_notify_port_status_for_all_user, port_no, 1 );
-//  expect_string( mock_notify_port_status_for_all_user, name, "Some port name1" );
-//  expect_value( mock_notify_port_status_for_all_user, up, true );
-
   expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
   expect_value( mock_notify_port_status_for_all_user, port_no, 2 );
   expect_string( mock_notify_port_status_for_all_user, name, "Port removed" );
@@ -290,6 +320,369 @@ test_feature_reply_then_update_ports() {
   teardown_topology_mgmt();
 }
 
+static void
+test_receive_switch_disconnected_then_notify_sw_status() {
+  setup_topology_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry *sw = update_sw_entry( &datapath_id );
+  assert_true( sw != NULL );
+  sw->up = true;
+  port_entry *p1 = update_port_entry( sw, 1, "Some port name1");
+  p1->up = true;
+  port_entry *p2 = update_port_entry( sw, 2, "Some port name2");
+  p2->up = true;
+
+  link_to* l = update_link_to(p1, &datapath_id, 2, true );
+  assert_true( l != NULL );
+
+  // send fake SW disconnected event to OFA I/F.
+  openflow_service_header_t data;
+  data.datapath_id = htonll( 0x1234 );
+  data.service_name_length = 0;
+  const size_t len = sizeof( openflow_service_header_t );
+  assert_true( send_message( OFA_SERVICE_NAME, MESSENGER_OPENFLOW_DISCONNECTED, &data, len ) );
+
+  // check notify to service mgmt
+  expect_value( mock_notify_link_status_for_all_user, from_dpid, 0x1234 );
+  expect_value( mock_notify_link_status_for_all_user, from_portno, 1 );
+  expect_value( mock_notify_link_status_for_all_user, to_dpid, 0x1234 );
+  expect_value( mock_notify_link_status_for_all_user, to_portno, 2 );
+  expect_value( mock_notify_link_status_for_all_user, up, false );
+
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 2 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Some port name2" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 1 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Some port name1" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  // stop messenger on sw notification
+  mock_notify_switch_status_for_all_user_max = 1;
+  expect_value( mock_notify_switch_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_switch_status_for_all_user, up, false );
+
+
+  // start
+  start_messenger();
+  start_event_handler();
+
+  // cleanup
+  int next_timeout_usec;
+  execute_timer_events( &next_timeout_usec );
+
+  sw = lookup_sw_entry( &datapath_id );
+  assert_true( sw == NULL );
+
+  teardown_topology_mgmt();
+}
+
+static void
+test_receive_port_add_status_then_notify_port_status() {
+  setup_topology_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry *sw = update_sw_entry( &datapath_id );
+  assert_true( sw != NULL );
+  sw->up = true;
+  port_entry *p1 = update_port_entry( sw, 1, "Some port name");
+  p1->up = true;
+  port_entry *p2 = update_port_entry( sw, 2, "Port removed");
+  p2->up = true;
+  port_entry *p4 = update_port_entry( sw, 4, "Port changed");
+  p4->up = true;
+
+  // send fake feature_reply event to OFA I/F.
+  buffer* buf = alloc_buffer();
+  openflow_service_header_t* ofs_header = append_back_buffer( buf, sizeof(openflow_service_header_t) );
+  ofs_header->datapath_id = htonll( 0x1234 );
+  ofs_header->service_name_length = htons( 0 );
+
+  struct ofp_port_status *port_status = append_back_buffer( buf, sizeof(struct ofp_port_status) );
+  memset( port_status, 0, sizeof(struct ofp_port_status) );
+  port_status->header.length = htons( sizeof(struct ofp_port_status) );
+  port_status->header.type = OFPT_PORT_STATUS;
+  port_status->header.version = OFP_VERSION;
+  port_status->header.xid = 0xDEADBEEF;
+  port_status->reason = OFPPR_ADD;
+
+  port_status->desc.port_no = htons( 3 );
+  port_status->desc.config = htonl( OFPPC_PORT_DOWN );
+  sprintf( port_status->desc.name, "Added port" );
+
+  assert_true( send_message( OFA_SERVICE_NAME, MESSENGER_OPENFLOW_MESSAGE, buf->data, buf->length ) );
+  free_buffer( buf );
+
+
+  // check notify to service mgmt
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 3 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Added port" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  // stop on last notification call
+  mock_notify_port_status_for_all_user_max = 1;
+
+  // pump message
+  start_messenger();
+  start_event_handler();
+
+  // clean up
+  port_entry *p = lookup_port_entry_by_port( sw, 1 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 2 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 3 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 4 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  delete_sw_entry( sw );
+
+  teardown_topology_mgmt();
+}
+
+static void
+test_receive_port_del_status_then_notify_port_status() {
+  setup_topology_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry *sw = update_sw_entry( &datapath_id );
+  assert_true( sw != NULL );
+  sw->up = true;
+  port_entry *p1 = update_port_entry( sw, 1, "Some port name");
+  p1->up = true;
+  port_entry *p2 = update_port_entry( sw, 2, "Port removed");
+  p2->up = true;
+  port_entry *p4 = update_port_entry( sw, 4, "Port changed");
+  p4->up = true;
+
+  // send fake feature_reply event to OFA I/F.
+  buffer* buf = alloc_buffer();
+  openflow_service_header_t* ofs_header = append_back_buffer( buf, sizeof(openflow_service_header_t) );
+  ofs_header->datapath_id = htonll( 0x1234 );
+  ofs_header->service_name_length = htons( 0 );
+
+  struct ofp_port_status *port_status = append_back_buffer( buf, sizeof(struct ofp_port_status) );
+  memset( port_status, 0, sizeof(struct ofp_port_status) );
+  port_status->header.length = htons( sizeof(struct ofp_port_status) );
+  port_status->header.type = OFPT_PORT_STATUS;
+  port_status->header.version = OFP_VERSION;
+  port_status->header.xid = 0xDEADBEEF;
+  port_status->reason = OFPPR_DELETE;
+
+  port_status->desc.port_no = htons( 2 );
+  port_status->desc.state = htonl( 0 );
+  sprintf( port_status->desc.name, "Port removed" );
+
+  assert_true( send_message( OFA_SERVICE_NAME, MESSENGER_OPENFLOW_MESSAGE, buf->data, buf->length ) );
+  free_buffer( buf );
+
+
+  // check notify to service mgmt
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 2 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Port removed" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  // stop on last notification call
+  mock_notify_port_status_for_all_user_max = 1;
+
+  // pump message
+  start_messenger();
+  start_event_handler();
+
+  // clean up
+  port_entry *p = lookup_port_entry_by_port( sw, 1 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 2 );
+  assert_true( p == NULL );
+
+
+  p = lookup_port_entry_by_port( sw, 3 );
+  assert_true( p == NULL );
+
+
+  p = lookup_port_entry_by_port( sw, 4 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  delete_sw_entry( sw );
+
+  teardown_topology_mgmt();
+}
+
+
+static void
+test_receive_port_mod_status_then_notify_port_status() {
+  setup_topology_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry *sw = update_sw_entry( &datapath_id );
+  assert_true( sw != NULL );
+  sw->up = true;
+  port_entry *p1 = update_port_entry( sw, 1, "Some port name");
+  p1->up = true;
+  port_entry *p2 = update_port_entry( sw, 2, "Port removed");
+  p2->up = true;
+  port_entry *p4 = update_port_entry( sw, 4, "Port changed");
+  p4->up = true;
+
+  link_to* l = update_link_to( p4, &datapath_id, 2, true );
+  assert_true( l != NULL );
+
+  // send fake feature_reply event to OFA I/F.
+  buffer* buf = alloc_buffer();
+  openflow_service_header_t* ofs_header = append_back_buffer( buf, sizeof(openflow_service_header_t) );
+  ofs_header->datapath_id = htonll( 0x1234 );
+  ofs_header->service_name_length = htons( 0 );
+
+  struct ofp_port_status *port_status = append_back_buffer( buf, sizeof(struct ofp_port_status) );
+  memset( port_status, 0, sizeof(struct ofp_port_status) );
+  port_status->header.length = htons( sizeof(struct ofp_port_status) );
+  port_status->header.type = OFPT_PORT_STATUS;
+  port_status->header.version = OFP_VERSION;
+  port_status->header.xid = 0xDEADBEEF;
+  port_status->reason = OFPPR_MODIFY;
+
+  port_status->desc.port_no = htons( 4 );
+  port_status->desc.state = htonl( OFPPS_LINK_DOWN );
+  sprintf( port_status->desc.name, "Port changed" );
+
+  assert_true( send_message( OFA_SERVICE_NAME, MESSENGER_OPENFLOW_MESSAGE, buf->data, buf->length ) );
+  free_buffer( buf );
+
+
+  // check notify to service mgmt
+  expect_value( mock_notify_link_status_for_all_user, from_dpid, 0x1234 );
+  expect_value( mock_notify_link_status_for_all_user, from_portno, 4 );
+  expect_value( mock_notify_link_status_for_all_user, to_dpid, 0x1234 );
+  expect_value( mock_notify_link_status_for_all_user, to_portno, 2 );
+  expect_value( mock_notify_link_status_for_all_user, up, false );
+
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 4 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Port changed" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  // stop on last notification call
+  mock_notify_port_status_for_all_user_max = 1;
+
+  // pump message
+  start_messenger();
+  start_event_handler();
+
+  // clean up
+  port_entry *p = lookup_port_entry_by_port( sw, 1 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 2 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 3 );
+  assert_true( p == NULL );
+
+
+  p = lookup_port_entry_by_port( sw, 4 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  delete_sw_entry( sw );
+
+  teardown_topology_mgmt();
+}
+
+static void
+test_receive_port_mod_status_port_no_then_notify_port_status() {
+  setup_topology_mgmt();
+
+  const uint64_t datapath_id = 0x1234;
+  sw_entry *sw = update_sw_entry( &datapath_id );
+  assert_true( sw != NULL );
+  sw->up = true;
+  port_entry *p1 = update_port_entry( sw, 1, "Some port name");
+  p1->up = true;
+  port_entry *p2 = update_port_entry( sw, 2, "Port removed");
+  p2->up = true;
+  port_entry *p4 = update_port_entry( sw, 4, "Port changed");
+  p4->up = true;
+
+
+  // send fake feature_reply event to OFA I/F.
+  buffer* buf = alloc_buffer();
+  openflow_service_header_t* ofs_header = append_back_buffer( buf, sizeof(openflow_service_header_t) );
+  ofs_header->datapath_id = htonll( 0x1234 );
+  ofs_header->service_name_length = htons( 0 );
+
+  struct ofp_port_status *port_status = append_back_buffer( buf, sizeof(struct ofp_port_status) );
+  memset( port_status, 0, sizeof(struct ofp_port_status) );
+  port_status->header.length = htons( sizeof(struct ofp_port_status) );
+  port_status->header.type = OFPT_PORT_STATUS;
+  port_status->header.version = OFP_VERSION;
+  port_status->header.xid = 0xDEADBEEF;
+  port_status->reason = OFPPR_MODIFY;
+
+  port_status->desc.port_no = htons( 3 );
+  sprintf( port_status->desc.name, "Port changed" );
+
+  assert_true( send_message( OFA_SERVICE_NAME, MESSENGER_OPENFLOW_MESSAGE, buf->data, buf->length ) );
+  free_buffer( buf );
+
+
+  // check notify to service mgmt
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 4 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Port changed" );
+  expect_value( mock_notify_port_status_for_all_user, up, false );
+
+  expect_value( mock_notify_port_status_for_all_user, datapath_id, 0x1234 );
+  expect_value( mock_notify_port_status_for_all_user, port_no, 3 );
+  expect_string( mock_notify_port_status_for_all_user, name, "Port changed" );
+  expect_value( mock_notify_port_status_for_all_user, up, true );
+
+  // stop on last notification call
+  mock_notify_port_status_for_all_user_max = 2;
+
+  // pump message
+  start_messenger();
+  start_event_handler();
+
+  // clean up
+  port_entry *p = lookup_port_entry_by_port( sw, 1 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 2 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+  p = lookup_port_entry_by_port( sw, 3 );
+  assert_true( p != NULL );
+  delete_port_entry( sw, p );
+
+
+  p = lookup_port_entry_by_port( sw, 4 );
+  assert_true( p == NULL );
+
+  delete_sw_entry( sw );
+
+  teardown_topology_mgmt();
+}
+
 /********************************************************************************
  * Run tests.
  ********************************************************************************/
@@ -300,10 +693,15 @@ main() {
       unit_test_setup_teardown( test_init_start_stop_finalize_topology_management, setup, teardown ),
       // test for set_switch_ready_handler( handle_switch_ready, NULL );
       unit_test( test_receive_switch_ready_then_notify_sw_status_and_request_features ),
-      // TODO test for set_switch_disconnected_handler( handle_switch_disconnected, NULL );
+      // test for set_switch_disconnected_handler( handle_switch_disconnected, NULL );
+      unit_test( test_receive_switch_disconnected_then_notify_sw_status ),
       // test for set_features_reply_handler( handle_switch_features_reply, NULL );
       unit_test( test_feature_reply_then_update_ports ),
-      // TODO test for set_port_status_handler( handle_port_status, NULL );
+      // test for set_port_status_handler( handle_port_status, NULL );
+      unit_test( test_receive_port_add_status_then_notify_port_status ),
+      unit_test( test_receive_port_del_status_then_notify_port_status ),
+      unit_test( test_receive_port_mod_status_then_notify_port_status ),
+      unit_test( test_receive_port_mod_status_port_no_then_notify_port_status ),
   };
 
   setup_leak_detector();
